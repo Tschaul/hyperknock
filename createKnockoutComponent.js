fae7361fa1cdb443f222b32527dd38d679f2dcd6
Julian Steinwachs <julian.steinwachs@fau.de>
2017-02-05 10:46:08 +0100

    fix component update

--- createKnockoutComponent.js
+++ createKnockoutComponent.js
@@ -2,7 +2,10 @@ var diff = require("./node_modules/virtual-dom/dist/virtual-dom.js").diff
 var patch = require("./node_modules/virtual-dom/dist/virtual-dom.js").patch
 var h = require("./node_modules/virtual-dom/dist/virtual-dom.js").h
 var createElement = require("./node_modules/virtual-dom/dist/virtual-dom.js").create
+
 var ko = require("./node_modules/knockout/build/output/knockout-latest.js");
+
+var deepEuqal = require("./node_modules/deep-equal/index.js")
 
 function s4(){
     return Math.floor((1 + Math.random()) * 0x10000)
@@ -16,7 +19,7 @@ module.exports = function(spec) {
 
     var Constructor = function(params) {
         this.params = params;
-        //console.log("constructor")
+        console.log("constructor")
     };
     Constructor.prototype.type = "Widget";
     //Constructor.prototype.name = componentId;
@@ -32,21 +35,26 @@ module.exports = function(spec) {
         var rootNode = createElement(currentNode)
 
         this.renderedObservable.subscribe(function(nextNode){
-            //console.log("event fired")
+            // console.log("event fired")
+            // console.log(currentNode,nextNode)
             var patches = diff(currentNode, nextNode)
-            //console.log("diffed")
+            // console.log("diffed")
+            // console.log(patches)
             rootNode = patch(rootNode, patches)
             //console.log("patched")
             currentNode = nextNode
         })
 
-        //console.log("init")
+        console.log("init")
 
         return rootNode;
     }
 
     Constructor.prototype.update = function(previous, domNode) {
-        //console.log("update",this.params)
+        console.log("update")
+        if(!deepEuqal(this.params,previous.params)){
+            return this.init();
+        }
         // return domNode;
     }
 
